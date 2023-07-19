9a3246c6c72254e3d941301b6ce10ed31a5f9185
Yanelin Ramos <mylifesubscribes@gmail.com>
2023-07-19 14:26:34 -0400

    first commit

---
+++ src/main/java/com/yanelinramos/SOVBackend/SovBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yanelinramos.SOVBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanelinramos/SOVBackend/SovBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanelinramos.SOVBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SovBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
